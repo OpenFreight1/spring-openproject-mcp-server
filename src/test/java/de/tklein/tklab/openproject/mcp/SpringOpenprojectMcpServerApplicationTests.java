7bad2fe9011533616dc481b671a0f09c1ad2d41a
Thomas Klein <thomas@t-klein.de>
2025-12-03 15:26:14 +0100

    initial after spring initializer

---
+++ src/test/java/de/tklein/tklab/openproject/mcp/SpringOpenprojectMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tklein.tklab.openproject.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenprojectMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
